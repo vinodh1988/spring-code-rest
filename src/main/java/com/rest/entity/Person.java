ba24e94642288bb15c4479b0333d95a2bbf1b309
vinodh1988 <k.c.vinodh@hotmail.com>
2025-07-01 15:58:10 +0530

    latest

---
+++ src/main/java/com/rest/entity/Person.java
@@ -0,0 +1,14 @@
+package com.rest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+private Integer sno;
+private String name;
+private String city;
+}
--- src/main/java/com/rest/services/DataService.java
+++ src/main/java/com/rest/services/DataService.java
@@ -1,9 +1,32 @@
 package com.rest.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.rest.entity.Person;
 
 @Service
 public class DataService {
+List<Person> persons = new ArrayList<>();
+
+   {
+	  persons.add(new Person(1, "John", "New York"));
+	  persons.add(new Person(2, "Jane", "Los Angeles"));
+	  persons.add(new Person(3, "Doe", "Chicago"));
+	  persons.add(new Person(4, "Smith", "Houston"));
+	  persons.add(new Person(5, "Emily", "Phoenix"));
+	  persons.add(new Person(6, "Michael", "Philadelphia"));
+	  persons.add(new Person(7, "Sarah", "San Antonio"));
+	  persons.add(new Person(8, "David", "San Diego"));
+	  persons.add(new Person(9, "Laura", "Dallas"));
+	  persons.add(new Person(10, "James", "San Jose"));
+   }
+
+   public List<Person> getPersons() {
+	  return persons;
+   }
  public String[] geNames() {
    return new String[] { 
 	 "John Doe", 
