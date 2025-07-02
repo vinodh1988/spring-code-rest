251a36ff732ac08e0984361f8d26b666a80e6ad1
vinodh1988 <k.c.vinodh@hotmail.com>
2025-07-02 14:46:23 +0530

    pathvariable

--- src/main/java/com/rest/controller/ProjectAPI.java
+++ src/main/java/com/rest/controller/ProjectAPI.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rest.entity.Project;
 import com.rest.services.ProjectService;
 import com.rest.utilities.RecordAlreadyExistsException;
+import com.rest.utilities.RecordNotFoundException;
 
 @RestController
 @RequestMapping("/api/v1/projects")
@@ -27,11 +30,22 @@ public class ProjectAPI {
 		return projectService.getAllProjects();
 	}
 	
+	@GetMapping("/{pno}")
+	public Project getProjectById(@PathVariable("pno") int pno) throws RecordNotFoundException { // Custom exception to handle record not found scenario
+		return projectService.getProjectByPno(pno);
+	}
+	
 	@PostMapping("")
 	public ResponseEntity<Project> addProject(@RequestBody Project project)
 	throws RecordAlreadyExistsException { // Custom exception to handle record already exists scenario
 		projectService.addProject(project);
 		return new ResponseEntity<>(project, HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("/{pno}")
+	public ResponseEntity<String> deleteProject(@PathVariable("pno") int pno) throws RecordNotFoundException {
+		projectService.deleteProject(pno);
+		return new ResponseEntity<>("Project deleted successfully", HttpStatus.OK);
 	}
 	
 }
--- src/main/java/com/rest/services/ProjectService.java
+++ src/main/java/com/rest/services/ProjectService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.rest.entity.Project;
 import com.rest.repository.ProjectRepository;
 import com.rest.utilities.RecordAlreadyExistsException;
+import com.rest.utilities.RecordNotFoundException;
 
 @Service
 public class ProjectService {
@@ -25,6 +26,20 @@ public void addProject(Project project) throws RecordAlreadyExistsException {
 	    if(p != null) 
 	    	throw new RecordAlreadyExistsException();
 	  projectRepository.save(project);// it works for both insert and update operations
+   }
+
+public Project getProjectByPno(int pno) throws RecordNotFoundException {
+	Project p = projectRepository.findByPno(pno);
+	if(p == null)
+		throw new RecordNotFoundException();
+		return p;
+   }
+
+public void deleteProject(int pno) throws RecordNotFoundException {
+	Project p = projectRepository.findByPno(pno);
+	if(p == null)
+		throw new RecordNotFoundException();
+	projectRepository.delete(p);
    }
 
 }
